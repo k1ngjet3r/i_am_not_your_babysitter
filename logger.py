f892ab9c7f1eea7729d0548b162efaf081b933d2
Jeter@work <43425262+k1ngjet3r@users.noreply.github.com>
2020-12-28 11:25:26 +0800

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+import time
+from datetime import datetime
+
+
+driver = webdriver.Chrome()
+driver.get('http://redmine.mdtc.cienet.com.cn:3000/projects/timesheet/issues')
+
+user_name = 'jeter.lin'
+password = '3qqLogan'
+
+user_field = driver.find_element_by_id('username').text
