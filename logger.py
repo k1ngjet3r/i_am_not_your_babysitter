56e96ceef3f68ace9734be361282821a9becd3a9
Jeter@work <43425262+k1ngjet3r@users.noreply.github.com>
2020-12-29 16:34:32 +0800

    Finalized the program

--- logger.py
+++ logger.py
@@ -47,45 +47,30 @@ class Logger:
         loggin_but = driver.find_element_by_xpath(
             '/html/body/div/div/div[3]/div[2]/div/form/table/tbody/tr[4]/td[2]/input')
         loggin_but.send_keys(Keys.RETURN)
-
-        # Go to the log-time directory
-        print('heading to the directory...')
-        step_1 = driver.find_element_by_xpath(
-            '/html/body/div/div/div[3]/div[1]/a[18]')
-        step_1.send_keys(Keys.RETURN)
-
-        step_2 = driver.find_element_by_xpath(
-            '/html/body/div/div/div[3]/div[2]/form[2]/div[2]/table/tbody/tr[8]/td[5]/a')
-        step_2.send_keys(Keys.RETURN)
-
-        print("click the Log-time button")
-        logtime = driver.find_element_by_xpath(
-            '/html/body/div/div/div[3]/div[2]/div[1]/a[2]')
-        logtime.send_keys(Keys.RETURN)
-
-        # Enter detail
-        print('Enter Hours -> 8 hours')
-        hour = driver.find_element_by_id('time_entry_hours')
-        hour.send_keys('8')
-
-        print('Enter Working City -> Taipei')
-        city = driver.find_element_by_id('time_entry_working_city')
-        city.send_keys('Taipei')
-
-        print('Select the activity -> MY22')
-        select_activity = Select(
-            driver.find_element_by_id('time_entry_activity_id'))
-        select_activity.select_by_index(8)
-
+        
         for i in range(self.duration):
-            sd = sd + dt.timedelta(days=i)
-            print('Enter date -> {}'.format(sd))
+            driver.get('http://redmine.mdtc.cienet.com.cn:3000/issues/32609/time_entries/new')
+            ed = sd + dt.timedelta(days=i)
+
+            # Enter detail
+            print('Enter date -> {}'.format(ed))
             driver.find_element_by_id('time_entry_spent_on').clear()
-            driver.find_element_by_id('time_entry_spent_on').send_keys(str(sd))
+            driver.find_element_by_id('time_entry_spent_on').send_keys(str(ed))
 
-            save_but = driver.find_elements_by_xpath('/html/body/div/div/div[3]/div[2]/form/input[2]')
-            save_but.send_keys(Keys.RETURN)
+            driver.find_element_by_id('time_entry_hours').send_keys('8')
 
+            driver.find_element_by_id('time_entry_working_city').send_keys('Taipei')
 
-log = Logger('jeter.lin', '3qqLogan', '20201214', 5)
+            select_activity = Select(
+                driver.find_element_by_id('time_entry_activity_id'))
+            select_activity.select_by_index(8)
+
+            driver.find_element_by_id('time_entry_spent_on').send_keys(Keys.ENTER)
+
+            print('complete logging day {}/{}'.format(i+1, self.duration))
+            print('---------------------------------------')
+            time.sleep(5)
+        print('Log time completed.')
+
+log = Logger('jeter.lin', '3qqLogan', '20201228', 4)
 log.logging()
