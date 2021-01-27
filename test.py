6c2912568746d12788ab74f79b277db2b6432bff
Jeter@work <43425262+k1ngjet3r@users.noreply.github.com>
2021-01-27 21:26:46 +0800

    Major update
    update the format of national_holiday.json
    added my22 leader logging function

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import time
 import datetime as dt
 import json
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
 
 with open('name.json', 'r') as f:
     name_list = json.load(f)
@@ -14,8 +14,8 @@ with open('name.json', 'r') as f:
 with open('national_holiday.json', 'r') as w:
     exception = json.load(w)
 
-national_holiday = ['2021-' +day for day in exception['holiday_2021']]
-make_up = ['2021-'+ day for day in exception['make_up']]
+national_holiday = ['2021-'+day for day in exception['holiday_2021']]
+make_up = ['2021-'+day for day in exception['make_up']]
 
 
 class Logger:
@@ -29,31 +29,31 @@ class Logger:
         return dt.date(year=y, month=m, day=d)
 
     def enter(self):
-        # Open the Redmine using Chrome
-        try:
-            driver.get(
-                'http://redmine.mdtc.cienet.com.cn:3000/projects/timesheet/issues')
-            time.sleep(3)
+    #     # Open the Redmine using Chrome
+    #     try:
+    #         driver.get(
+    #             'http://redmine.mdtc.cienet.com.cn:3000/projects/timesheet/issues')
+    #         time.sleep(3)
 
-        # If the wifi connection is wrong, raise the error message
-        except:
-            print("I'm offline so I can't do that!, Please check your WiFi setting!")
-            raise SystemExit()
+    #     # If the wifi connection is wrong, raise the error message
+    #     except:
+    #         print("I'm offline so I can't do that!, Please check your WiFi setting!")
+    #         raise SystemExit()
 
         # For enter the username and password in terminal
         username, password, url = self.un_pw()
-        try:
-            print('Entering the user info...')
-            user_field = driver.find_element_by_id('username')
-            user_field.send_keys(username)
-            pw_field = driver.find_element_by_id('password')
-            pw_field.send_keys(password)
-            loggin_but = driver.find_element_by_xpath(
-                '/html/body/div/div/div[3]/div[2]/div/form/table/tbody/tr[4]/td[2]/input')
-            loggin_but.send_keys(Keys.RETURN)
-        except:
-            print('Fail to log in, please check your username and password')
-            self.un_pw()
+        # try:
+        #     print('Entering the user info...')
+        #     user_field = driver.find_element_by_id('username')
+        #     user_field.send_keys(username)
+        #     pw_field = driver.find_element_by_id('password')
+        #     pw_field.send_keys(password)
+        #     loggin_but = driver.find_element_by_xpath(
+        #         '/html/body/div/div/div[3]/div[2]/div/form/table/tbody/tr[4]/td[2]/input')
+        #     loggin_but.send_keys(Keys.RETURN)
+        # except:
+        #     print('Fail to log in, please check your username and password')
+        #     self.un_pw()
 
         start_date = input(
             'The first day you want to log (format: 20201019): ')
@@ -83,12 +83,11 @@ class Logger:
                 self.leader_logging(start_date, duration, url)
             else:
                 self.logging(start_date, duration, url)
-
         else:
             print('Goodbye!')
 
     def un_pw(self):
-        url = ''
+        
         username = input('Enter your username: ')
         password = input('and your password: ')
 
@@ -117,10 +116,12 @@ class Logger:
                 self.enter_info(entered_date, duration, url[0], i)
             else:
                 self.enter_info(entered_date, duration, url[1], i)
+            
+            
 
     def enter_info(self, date, duration, url, day_i):
         # go to the logging page
-        driver.get(url) 
+        print(url) 
 
         if (date.weekday() == 5 or date.weekday() == 6) and str(date) not in make_up:
             print('{} is weekend, Pass'.format(date))
@@ -132,24 +133,24 @@ class Logger:
 
         else:
             print('Enter date -> {}'.format(date))
-            driver.find_element_by_id('time_entry_spent_on').clear()
-            driver.find_element_by_id(
-                'time_entry_spent_on').send_keys(str(date))
+            # driver.find_element_by_id('time_entry_spent_on').clear()
+            # driver.find_element_by_id(
+            #     'time_entry_spent_on').send_keys(str(date))
 
-            # Entering the working time: 8 hours
-            driver.find_element_by_id('time_entry_hours').send_keys('8')
+            # # Entering the working time: 8 hours
+            # driver.find_element_by_id('time_entry_hours').send_keys('8')
 
-            # Entering the work location: Taipei
-            driver.find_element_by_id(
-                'time_entry_working_city').send_keys('Taipei')
+            # # Entering the work location: Taipei
+            # driver.find_element_by_id(
+            #     'time_entry_working_city').send_keys('Taipei')
 
-            # Entering the work type: MY22
-            select_activity = Select(
-                driver.find_element_by_id('time_entry_activity_id'))
-            select_activity.select_by_index(8)
+            # # Entering the work type: MY22
+            # select_activity = Select(
+            #     driver.find_element_by_id('time_entry_activity_id'))
+            # select_activity.select_by_index(8)
 
-            driver.find_element_by_id(
-                'time_entry_spent_on').send_keys(Keys.ENTER)
+            # driver.find_element_by_id(
+            #     'time_entry_spent_on').send_keys(Keys.ENTER)
 
             print('complete logging day {}/{}'.format(day_i + 1, duration))
             print('---------------------------------------')
